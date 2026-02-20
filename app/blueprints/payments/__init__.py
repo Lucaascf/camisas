9d68ba19264a28052164485626db63eff80d8bc3
lucas <lucascf1302@gmail.com>
2026-02-20 18:42:09 -0300

    implementando o mercado pago

--- app/blueprints/payments/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Blueprint de pagamentos - Mercado Pago."""
-
-from flask import Blueprint
-
-payments_bp = Blueprint('payments', __name__, url_prefix='/payments')
-
-from app.blueprints.payments import rotas
